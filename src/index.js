2d8778a5c327376be0bc694013504003f46ccd88
Ivan Perez <joseivanperezdiaz1@gmail.com>
2020-09-23 15:05:05 -0500

    useMemo 23.09.20 15:40

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoHook } from './components/06-memos/MemoHook';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
@@ -8,9 +9,10 @@ import ReactDOM from 'react-dom';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './components/04-useRef/FocusScreen';
 // import { RealExampleRef } from './components/04-useRef/RealExampleRef';
-import { Layout } from './components/05-useLayouteffect/Layout';
+// import { Layout } from './components/05-useLayouteffect/Layout';
+// import { Memorize } from './components/06-memos/Memorize';
 
 ReactDOM.render(
-  <Layout />,
+  <MemoHook />,
   document.getElementById('root')
 );
