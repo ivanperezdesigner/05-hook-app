e5a5223fa6c40d8939abd9050ecb07e8dd3c31b3
Ivan Perez <joseivanperezdiaz1@gmail.com>
2020-09-24 22:00:08 -0500

    useReducer 24.09.20 22:00

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -2,26 +2,57 @@ import React, { useReducer } from 'react'
 import './styles.css'
 import { todoReducer } from './todoReducer'
 
-const initialState = {
+const initialState = [{
     id: new Date().getTime(),
-    desc: 'Aprender React',
+    desc: 'Learn React',
     done: false
-}
+}]
 
 export const TodoApp = () => {
-
     const [ todos ] = useReducer(todoReducer, initialState)
     console.log(todos)
 
     return (
         <div>
-            <h1>Todo App</h1>
+            <h1>Todo App <small>({ todos.length })</small></h1>
             <hr/>
-            <ul>
-                <li>Hola</li>
-                <li>Mundo</li>
-                <li>Hola de nuevo</li>
-            </ul>
+            <div className='row'>
+                <div className='col-7'>
+                    <ul className='list-group list-group-flush'>
+                    {
+                        todos.map( (todo, i) => (
+                            <li
+                                className='list-group-item'
+                                key={todo.id}
+                            >
+                                <p className='text-center'>{i + 1}. {todo.desc}</p>
+                                <button className='btn btn-outline-danger'>
+                                    Delete
+                                </button>
+                            </li>
+                        ))
+                    }
+                    </ul>
+                </div>
+                <div className='col-5'>
+                    <h4>Add Tasks</h4>
+                    <hr/>
+                    <form>
+                        <input
+                            type='text'
+                            placeholder='Next task...'
+                            autoComplete='off'
+                            name='description'
+                            className='form-control'
+                        />
+                        <button
+                            className='btn btn-outline-success mt-1 btn-block'
+                        >
+                            Add task
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
